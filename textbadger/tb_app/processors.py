c04f992f364830ad69de52019cb097d831cf9a6a
Abe Gong <agong@umich.edu>
2012-06-18 16:41:58 -0400

    * Home/about page     * Remove ref to Double-J     * Add message on "forgot your login info?"

--- textbadger/tb_app/processors.py
+++ textbadger/tb_app/processors.py
@@ -7,8 +7,8 @@ def admin_names(request):
     if len(names) == 0:
         admin_names = "This instance of TextBadger has no administrators yet."
     elif len(names) == 1:
-        admin_names = "This instance of TextBadger is administered by "+names[0]
+        admin_names = "This instance of TextBadger is administered by "+names[0]+"."
     elif len(names) > 1:
-        admin_names = "This instance of TextBadger is administered by "+", ".join(names[:-1])+" and "+names[-1]
+        admin_names = "This instance of TextBadger is administered by "+", ".join(names[:-1])+" and "+names[-1]+"."
 
     return {'admin_names': admin_names}
