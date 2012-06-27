2d9236c54a2bb420d61319fe5962c6fe53f40192
Joshua Walker <joshua@snowandbones.com>
2012-06-27 15:51:52 -0400

    documented + clean

--- textbadger/tb_app/management/commands/loadfixtures.py
+++ textbadger/tb_app/management/commands/loadfixtures.py
@@ -8,6 +8,7 @@ from bson import json_util
 #import pymongo.json_util
 
 class Command(management.BaseCommand):
+    """Flush and initialize, or re-initialize, the database."""
     args = '<...>'
     help = 'Loads database fixtures'
 
@@ -20,7 +21,6 @@ class Command(management.BaseCommand):
 
         #Get DB connection
         conn = connections["default"]
-#        print list(conn.get_collection("auth_user").find())
 
         #Get all fixtures
         print 'Loading fixtures from', settings.PROJECT_PATH+'/fixtures/*'
@@ -30,15 +30,7 @@ class Command(management.BaseCommand):
             name = f.split('/')[-1].split('.')[0]
             print '\tLoding fixtures for collection', name, '...'
             contents = json.loads(file(f).read(), object_hook=json_util.object_hook)
-#            print json.dumps(contents, indent=2, default=json_util.default)
             conn.get_collection(name).insert(contents)
-
-
-#        print connections.databases["default"]#.__dict__
-#        conn = connections['my_db_alias']
-#        print conn
-#        eggs_collection = database_wrapper.get_collection('eggs')
-#        eggs_collection.find_and_modify(...)
 
         management.call_command('syncdb', interactive=False)
 
